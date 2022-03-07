83036947eadf7e007e942b00c59edd185122cbbd
Hariharan-SV` <harikumar20112000@gmail.com>
2022-03-07 11:22:54 +0530

    Initial Setup

---
+++ src/com/company/Main.java
@@ -0,0 +1,160 @@
+package com.company;
+import java.util.Random;
+import java.time.Duration;
+import java.time.Instant;
+
+public class Main {
+
+    // Main driver method
+    public static long mult(long x, long y) {
+
+        // Checking only if input is within range
+        if (x < 10 && y < 10) {
+
+            // Multiplying the inputs entered
+            return x * y;
+        }
+
+        // Declaring variables in order to
+        // Find length of both integer
+        // numbers x and y
+        int noOneLength = numLength(x);
+        int noTwoLength = numLength(y);
+
+        // Finding maximum length from both numbers
+        // using math library max function
+        int maxNumLength
+                = Math.max(noOneLength, noTwoLength);
+
+        // Rounding up the divided Max length
+        Integer halfMaxNumLength
+                = (maxNumLength / 2) + (maxNumLength % 2);
+
+        // Multiplier
+        long maxNumLengthTen
+                = (long)Math.pow(10, halfMaxNumLength);
+
+        // Compute the expressions
+        long a = x / maxNumLengthTen;
+        long b = x % maxNumLengthTen;
+        long c = y / maxNumLengthTen;
+        long d = y % maxNumLengthTen;
+
+
+        // Compute all mutilpying variables
+        // needed to get the multiplication
+        long z0 = mult(a, c);
+        long z1 = mult(a + b, c + d);
+        long z2 = mult(b, d);
+
+        long ans = (z0 * (long)Math.pow(10, halfMaxNumLength * 2) +
+                ((z1 - z0 - z2) * (long)Math.pow(10, halfMaxNumLength) + z2));
+
+        return ans;
+
+    }
+
+    // Method 1
+    // To calculate length of the number
+    public static int numLength(long n)
+    {
+        int noLen = 0;
+        while (n > 0) {
+            noLen++;
+            n /= 10;
+        }
+
+        // Returning length of number n
+        return noLen;
+    }
+
+    // Method 2
+    // Main driver function
+    public static void main(String[] args)
+    {
+        // Showcasing karatsuba multiplication
+
+        // Case 1: Big integer lengths
+        long  start = System.currentTimeMillis();
+        long expectedProduct = 12345678 * 56781234;
+        long end = System.currentTimeMillis();
+        long timeElapsed = end - start;
+        System.out.println("Sequential time : "+ timeElapsed +" milliseconds");
+
+        start = System.currentTimeMillis();
+        long actualProduct = mult(12345678, 56781234);
+        end = System.currentTimeMillis();
+        timeElapsed = end - start;
+        System.out.println("Parallel time : "+ timeElapsed +" milliseconds");
+        // Printing the expected and corresponding actual product
+        System.out.println("Expected 1 : " + expectedProduct);
+        System.out.println("Actual 1 : " + actualProduct + "\n\n");
+
+        assert(expectedProduct == actualProduct);
+
+        start = System.currentTimeMillis();
+        expectedProduct = 102102102 * 313313313;
+        end = System.currentTimeMillis();
+        timeElapsed = end - start;
+        System.out.println("Sequential time : "+ timeElapsed +" milliseconds");
+
+        start = System.currentTimeMillis();
+        actualProduct = mult(102102102, 313313313);
+        end = System.currentTimeMillis();
+        timeElapsed = end - start;
+        System.out.println("Parallel time : "+ timeElapsed +" milliseconds");
+
+        System.out.println("Expected 2 : " + expectedProduct);
+        System.out.println("Actual 2 : " + actualProduct + "\n\n");
+
+        assert(expectedProduct == actualProduct);
+
+        start = System.currentTimeMillis();
+        expectedProduct = 13451234 * 63456123;
+        end = System.currentTimeMillis();
+        timeElapsed = end-start;
+        System.out.println("Sequential time : "+ timeElapsed +" milliseconds");
+
+        start = System.currentTimeMillis();
+        actualProduct = mult(13451234, 63456123);
+        end = System.currentTimeMillis();
+        timeElapsed = end - start;
+        System.out.println("Parallel time : "+ timeElapsed +" milliseconds");
+
+        System.out.println("Expected 3 : " + expectedProduct);
+        System.out.println("Actual 3 : " + actualProduct + "\n\n");
+
+        assert(expectedProduct == actualProduct);
+
+        /*
+        Integer x = null;
+        Integer y = null;
+        Integer MAX_VALUE = 10000;
+
+        // Boe creating an object of random class
+        // inside main() method
+        Random r = new Random();
+
+        for (int i = 0; i < MAX_VALUE; i++) {
+            x = (int) r.nextInt(MAX_VALUE);
+            y = (int) r.nextInt(MAX_VALUE);
+
+            expectedProduct = x * y;
+
+            if (i == 9999) {
+
+                // Prove assertions catch the bad stuff.
+                expectedProduct = 1;
+            }
+            actualProduct = mult(x, y);
+
+            // Again printing the expected and
+            // corresponding actual product
+            System.out.println("Expected: " + expectedProduct);
+            System.out.println("Actual: " + actualProduct + "\n\n");
+
+            assert(expectedProduct == actualProduct);
+        }
+       */
+    }
+}
